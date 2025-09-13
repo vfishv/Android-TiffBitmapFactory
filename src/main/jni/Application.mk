2830049b80e8e3dbf2360e7bfa815484af0d647e
vfishv <vfishv@gmail.com>
2025-09-13 17:51:45 +0800

    Update Application.mk

--- src/main/jni/Application.mk
+++ src/main/jni/Application.mk
@@ -4,7 +4,7 @@ APP_MODULES      := tiff png jpeg tifffactory tiffsaver tiffconverter
 
 APP_PLATFORM=android-16
 
-APP_ABI := arm64-v8a, armeabi-v7a
+APP_ABI := all
 APP_SUPPORT_FLEXIBLE_PAGE_SIZES := true
 
 #APP_OPTIM := debug
