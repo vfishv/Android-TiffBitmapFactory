da09bc4b3477429b23c5cb9e0717f829a82605e3
vfishv <vfishv@gmail.com>
2025-09-13 17:39:42 +0800

    APP_SUPPORT_FLEXIBLE_PAGE_SIZES := true

--- src/main/jni/Application.mk
+++ src/main/jni/Application.mk
@@ -3,6 +3,7 @@ APP_STL := c++_static
 APP_MODULES      := tiff png jpeg tifffactory tiffsaver tiffconverter
 
 APP_PLATFORM=android-16
+APP_SUPPORT_FLEXIBLE_PAGE_SIZES := true
 
 APP_ABI := arm64-v8a, armeabi-v7a
 
